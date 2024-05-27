4291dcd1a450446f111bc47def49820f073c90d1
Benjamin Killisch <bkillisch@gmail.com>
2024-05-27 19:57:21 +0200

    add pipeline with test project

---
+++ src/components/HelloWorld.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from "@vue/test-utils";
+import HelloWorld from './HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('increments count when button is clicked', async () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg: 'Test Message',
+      },
+    })
+
+    expect(wrapper.find('button').text()).toBe("count is 0")
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.find('button').text()).toBe("count is 1")
+  })
+})
