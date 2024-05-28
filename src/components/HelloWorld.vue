eb7a5212b15389c9f6415f8d532da349dc1fe674
Benjamin Killisch <Benjamin.Killisch@relaxdays.de>
2024-05-28 15:05:19 +0200

    setup project with backend call

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,31 +4,44 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const weatherForecasts = ref([])
+
+async function getForecasts() {
+  count.value++;
+  try {
+    const response = await fetch(import.meta.env.VITE_BACKEND_URL + '/WeatherForecast?count=' + count.value, {
+      headers: {
+        'Accept': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'cors': 'no-cors'
+      },
+    })
+    weatherForecasts.value = await response.json()
+  } catch (error) {
+    console.error('Failed to fetch weather forecasts:', error)
+  }
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="getForecasts()">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>
+    <h2>Weather Forecasts</h2>
+    <ul>
+      <li v-for="forecast in weatherForecasts" :key="forecast.date">
+        {{ forecast.date }} - {{ forecast.temperatureC }}°C - {{ forecast.summary }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
