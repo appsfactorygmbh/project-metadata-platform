50475c64719d18c033fbd9cbc5b6adaf91ce8bd3
Arthur <arthur.praters@stud.htwk-leipzig.de>
2024-06-06 18:22:20 +0200

    add table (col drop when width reduced)

---
+++ src/components/tableComponent.test.ts
@@ -0,0 +1,7 @@
+import { mount } from "@vue/test-utils";
+import Table from "../components/tableComponent.vue";
+import { describe, it, expect } from "vitest";
+
+describe("tableComponent.vue", () => {
+    it("renders correctly with ")
+})
