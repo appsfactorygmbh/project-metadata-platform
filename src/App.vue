7554a549d303a12b949557d06eb32f6ea572b661
Jonas Nathanael Fleischer <jonas_nathanael.fleischer@stud.htwk-leipzig.de>
2024-06-06 17:26:46 +0200

    Added Theme Provider and tsconfig absolute import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue';
+  import { ThemeProvider } from '@/router/Provider';
+  import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ThemeProvider>
+    <a-button type="primary">Primary Button</a-button>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </ThemeProvider>
 </template>
 
 <style scoped>
