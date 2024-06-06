b795f7c767431a984eb8271fdec8c1e0859e428f
Oscar Friske <oscar.friske@stud.htwk-leipzig.de>
2024-06-06 20:35:44 +0200

    put url functions into separate file

--- src/components/Plugin/editURL.ts
+++ src/components/Plugin/editURL.ts
@@ -1,8 +1,13 @@
 export const cutAfterTLD = (url: string): string => {
-    const regex = /^(https?:\/\/)?([^\/]+(\.[a-z]{2,}))/i;
-    const match = url.match(regex);
-    if (match) {
-      return match[0];
-    }
-    return url;
-}
+  const regex = /^(https?:\/\/)?([^/]+(\.[a-z]{2,}))/i;
+  const match = url.match(regex);
+  if (match) {
+    return match[0];
+  }
+  return url;
+};
+
+export const createFaviconURL = (tld: string) => {
+  // return 'https://www.google.com/s2/favicons?domain=${tld}&sz=128';
+  return 'https://www.google.com/s2/favicons?domain=' + tld + '&sz=128';
+};
