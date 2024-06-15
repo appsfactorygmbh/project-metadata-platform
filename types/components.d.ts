6ee76984610036ca917a6e69f62e273f88c221be
Nicolas Grzywacz <nicolas_adam.grzywacz@stud.htwk-leipzig>
2024-06-15 16:15:47 +0200

    created vue-structure for new component

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,8 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: (typeof import('./../src/components/HelloWorld.vue'))['default'];
-    RouterLink: (typeof import('vue-router'))['RouterLink'];
-    RouterView: (typeof import('vue-router'))['RouterView'];
+    HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SearchBar: typeof import('./../src/components/Searchbar/SearchBar.vue')['default']
   }
 }
