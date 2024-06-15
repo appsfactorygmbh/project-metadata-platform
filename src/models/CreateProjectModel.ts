fe5109d7e725eda08a4d822d5495c77252e74300
Oscar Friske <oscar.friske@stud.htwk-leipzig.de>
2024-06-15 18:07:59 +0200

    added put request to productService (oscar & rami)

---
+++ src/models/CreateProjectModel.ts
@@ -0,0 +1,13 @@
+//     "ProjectName": "",
+//     "BusinessUnit": "",
+//     "TeamNumber": "",
+//     "Department": "",
+//     "ClientName": ""
+
+export type CreateProject = {
+  projectName: string,
+  businessUnit: string,
+  teamNumber: string,
+  department: string,
+  clientname: string,
+}
---
+++ src/services/ProjectService.ts
@@ -0,0 +1,42 @@
+import { Project } from 'models/TableModel';
+import { CreateProject } from 'models/CreateProjectModel';
+
+class ProjectsService {
+  fetchProjects = async () => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URL + '/projects',
+      );
+
+      const data: Project[] = await response.json();
+
+      return data;
+    } catch (err) {
+      console.error('Error fetching projects: ' + err);
+    }
+  };
+
+  addProject = async (projectData: CreateProject) => {
+    try {
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(projectData)
+      });
+
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
+      const responseData = await response.json();
+      console.log("Success:", responseData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+}
+
+const projectsService = new ProjectsService();
+export { projectsService };
