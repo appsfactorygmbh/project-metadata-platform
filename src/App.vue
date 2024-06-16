c849760cad60c1cb8a5f20f7e6e2df00fac29027
Jonas Nathanael Fleischer <jonas_nathanael.fleischer@stud.htwk-leipzig.de>
2024-06-17 01:10:23 +0200

    add pinia and router to main ts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
-  import { ThemeProvider } from './router/Provider';
-  import HelloWorld from './components/HelloWorld.vue';
+  import { ThemeProvider } from '@/router/Provider';
 </script>
 
 <template>
   <ThemeProvider>
-    <a-button type="primary">Primary Button</a-button>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
+    <header>
+      <a-button type="primary">Primary Button</a-button>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+    </header>
+    <RouterView />
   </ThemeProvider>
 </template>
 
