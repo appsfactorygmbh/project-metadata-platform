2712ca7fa03c53b64c90b46fab05a27cae4a114b
Jonas Nathanael Fleischer <jonas_nathanael.fleischer@stud.htwk-leipzig.de>
2024-06-17 02:44:44 +0200

    added relative import for view folder to reduce any upcoming changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import SplitView from '@/views/SplitView';
+import { SplitView } from '@/views';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
