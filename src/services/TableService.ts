61d3928fc2fb69bfbc642de56e75b5ba1341c372
Arthur Praters <arthur.praters@stud.htwk-leipzig.de>
2024-06-18 07:43:13 +0200

    fix yarn format

--- src/services/TableService.ts
+++ src/services/TableService.ts
@@ -4,13 +4,13 @@ class ProjectsService {
   fetchProjects = async () => {
     try {
       const response = await fetch(
-        import.meta.env.VITE_BACKEND_URL + '/projects',
-        //"./src/components/Table/test.json"
+        //import.meta.env.VITE_BACKEND_URL + '/projects',
+        './src/components/Table/test.json',
       );
 
       const data: Project[] = await response.json();
       console.log(data);
-      
+
       return data;
     } catch (err) {
       console.error('Error fetching projects: ' + err);
