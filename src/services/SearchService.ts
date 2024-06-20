b43b0568933ce75c2a3cc619f6623f668849ae77
Nicolas Grzywacz <nicolas_adam.grzywacz@stud.htwk-leipzig>
2024-06-20 11:24:51 +0200

    URL moved outside of the try block, check response code added, console.log changed into console.error

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -3,19 +3,22 @@ import type { SearchType } from '../models/SearchModel';
 export async function searchProjects(
   searchQuery: string = '',
 ): Promise<SearchType[]> {
+  const url = `${import.meta.env.VITE_BACKEND_URL}/Projects?search=${searchQuery}`;
   try {
-    const url = `${import.meta.env.VITE_BACKEND_URL}/Projects?search=${searchQuery}`;
-
+    
     //Fetching
     console.log('Fetching URL:', url);
     const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
 
     //Response
     const data: SearchType[] = await response.json();
     console.log('Response data:', data);
     return data;
   } catch (err) {
-    console.log('Error searching projects: ' + err);
+    console.error('Error searching projects:', err);
     return [];
   }
 }
