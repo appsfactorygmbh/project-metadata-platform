d67d67ef7b30d654c74a181375d2f61a0a6c5c42
Arthur Praters <arthur.praters@stud.htwk-leipzig.de>
2024-06-21 12:34:33 +0200

    move ProjectInformation into its view

---
+++ src/views/ProjectInformationView/__test__/ProjectInformation.test.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect, vi } from 'vitest';
+import { flushPromises, mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import ProjectInformation from '../ProjectInformationView.vue';
+
+setActivePinia(createPinia());
+
+const data = {
+  projectName: 'Heute Show',
+  department: 'IT',
+  clientName: 'ZDF',
+  businessUnit: 'BU Health',
+  teamNumber: 42,
+};
+
+describe('projectView.vue', () => {
+  const mockResponse = {
+    ok: true,
+    statusText: 'Ok',
+    json: async () => data,
+  } as Response;
+  globalThis.fetch = vi.fn().mockResolvedValue(mockResponse);
+
+  function generateWrapper() {
+    return mount(ProjectInformation, {
+      propsData: {
+        paneWidth: 1000,
+        projectId: 100,
+        isTest: true,
+      },
+      global: {
+        stubs: {
+          PluginView: {
+            template: '<span />',
+          },
+        },
+      },
+    });
+  }
+
+  it('renders the project information correctly', async () => {
+    const wrapper = generateWrapper();
+    await flushPromises();
+
+    expect(wrapper.find('.projectName').text()).toBe('Heute Show');
+    expect(wrapper.findAll('.projectInfo')[0].text()).toBe('BU Health');
+    expect(wrapper.findAll('.projectInfo')[1].text()).toBe('42');
+    expect(wrapper.findAll('.projectInfo')[2].text()).toBe('IT');
+    expect(wrapper.findAll('.projectInfo')[3].text()).toBe('ZDF');
+  });
+});
