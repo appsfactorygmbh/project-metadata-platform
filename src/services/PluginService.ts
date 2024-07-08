3862e7833d3f67d0212ef8d08896f4bac6d25403
Rami Gasim Elseed <rami.gasim_elseed@stud.htwk-leipzig.de>
2024-07-08 21:07:09 +0200

    add mock of fetchGlobalPluginData in PluginService actual function body is implemented but not tested

--- src/services/PluginService.ts
+++ src/services/PluginService.ts
@@ -23,6 +23,30 @@ class PluginService {
     //TODO: implement function
     console.log(plugin);
   };
+
+  fetchGlobalPluginData = async (pluginId: number) => {
+    //   try {
+    //     const response = await fetch(
+    //       import.meta.env.VITE_BACKEND_URL + '/plugins/' + pluginId.toString(),
+    //     );
+    //     if (!response.ok)
+    //       throw new Error('Error when trying to fetch global Plugin');
+    //     const data = await response.json();
+    //     console.log('data from fetch: ', data);
+    //     return data;
+    //   } catch (error) {
+    //     console.log(error);
+    //     return [];
+    //   }
+    // };
+
+    /* mock of function, actual function body is above */
+    return {
+      id: pluginId,
+      pluginName: 'Test Plugin',
+      keys: { key1: 'value1', key2: 'value2' },
+    };
+  };
 }
 
 const pluginService = new PluginService();
