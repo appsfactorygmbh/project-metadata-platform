0244cd05b0a0a4b66aeab10a3d96db3d5e153ced
Arthur Praters <arthur.praters@stud.htwk-leipzig.de>
2024-07-10 23:02:09 +0200

    Update ProjectSearchView.test.ts store setup

--- src/views/ProjectSearchView/__test__/ProjectSearchView.test.ts
+++ src/views/ProjectSearchView/__test__/ProjectSearchView.test.ts
@@ -2,11 +2,33 @@ import { flushPromises, mount } from '@vue/test-utils';
 import ProjectSearchView from '../ProjectSearchView.vue';
 import { describe, it, expect } from 'vitest';
 import { createTestingPinia } from '@pinia/testing';
-import { useProjectStore } from '@/store';
+import { usePluginsStore, useProjectStore, useSearchStore } from '@/store';
 import { createPinia, setActivePinia } from 'pinia';
 import _ from 'lodash';
-import { projectsStoreSymbol } from '@/store/injectionSymbols';
+import {
+  pluginStoreSymbol,
+  projectsStoreSymbol,
+} from '@/store/injectionSymbols';
 import router from '@/router';
+
+const testProject = [
+  {
+    id: 100,
+    projectName: 'Heute Show',
+    clientName: 'ZDF',
+    businessUnit: 'BU Health',
+    teamNumber: 42,
+  },
+];
+
+const testProjectInfo = {
+  id: 100,
+  projectName: 'Heute Show',
+  department: 'IT',
+  clientName: 'ZDF',
+  businessUnit: 'BU Health',
+  teamNumber: 42,
+};
 
 describe('ProjectSearchView.vue', () => {
   setActivePinia(createPinia());
@@ -16,11 +38,20 @@ describe('ProjectSearchView.vue', () => {
       plugins: [
         createTestingPinia({
           stubActions: false,
+          initialState: {
+            project: {
+              project: testProjectInfo,
+            },
+            project_search: {
+              baseSet: testProject,
+            },
+          },
         }),
       ],
       global: {
         provide: {
           [projectsStoreSymbol as symbol]: useProjectStore(),
+          [pluginStoreSymbol as symbol]: usePluginsStore(),
         },
         plugins: [router],
       },
@@ -33,7 +64,18 @@ describe('ProjectSearchView.vue', () => {
 
   const wrapper = generateWrapper(800);
 
-  it('renders correctly with 4 columns', () => {
+  const searchStore = useSearchStore('project');
+
+  const loadData = async () => {
+    searchStore.setBaseSet(testProject);
+    searchStore.setSearchQuery('');
+    await flushPromises();
+  };
+
+  it('renders correctly with 4 columns', async () => {
+    await loadData();
+    console.log(wrapper.html());
+
     expect(wrapper.findAll('.ant-table-column-sorters')).toHaveLength(4);
   });
 
