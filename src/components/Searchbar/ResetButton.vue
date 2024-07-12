4aee69bfd5d5d05aab7d39954e01b886e9d65ee7
Chaimaa Raif <chaimaa.raif@stud.htwk-leipzig.de>
2024-07-12 13:40:19 +0200

    ResetButton.vue

---
+++ src/components/Searchbar/ResetButton.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  import type { FloatButtonModel } from '@/components/Button/FloatButtonModel';
+  import { UndoOutlined } from '@ant-design/icons-vue';
+
+  const button: FloatButtonModel = {
+    name: 'ResetFilterButton',
+    icon: UndoOutlined,
+    status: 'activated',
+    tooltip: 'Click here to reset to search filter',
+  };
+</script>
+
+<template>
+  <FloatingButton :button="button" class="reset" />
+</template>
+
+<style scoped>
+  .reset {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+  }
+</style>
