aaab5231370bc6d36108633cb5dd2a99012c29c2
Rami Gasim Elseed <rami.gasim_elseed@stud.htwk-leipzig.de>
2024-07-18 14:15:37 +0200

    fix merge: double import of GlobalPluginProvider

--- src/router/Provider/ProviderCollection.vue
+++ src/router/Provider/ProviderCollection.vue
@@ -2,7 +2,6 @@
   import {
     ProjectProvider,
     PluginProvider,
-    GlobalPluginProvider,
     ProjectEditProvider,
   } from '@/router/Provider';
   import GlobalPluginProvider from './GlobalPluginProvider.vue';
