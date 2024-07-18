c7b6faa177b0c6e8eea2b13d395c3dd349e700c8
Rami Gasim Elseed <rami.gasim_elseed@stud.htwk-leipzig.de>
2024-07-18 14:32:17 +0200

    fix merge: double import of GlobalPluginProvider

--- src/models/Plugin/index.ts
+++ src/models/Plugin/index.ts
@@ -2,4 +2,9 @@ import type { PluginModel } from './PluginModel';
 import type { PluginEditModel } from './PluginEditModel';
 import type { GlobalPluginModel, GlobalPluginKey } from './GlobalPluginModel';
 
-export type { PluginModel, GlobalPluginModel, GlobalPluginKey, PluginEditModel };
+export type {
+  PluginModel,
+  GlobalPluginModel,
+  GlobalPluginKey,
+  PluginEditModel,
+};
