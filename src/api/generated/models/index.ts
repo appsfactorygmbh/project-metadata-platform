63ea1437e1bbcc6af46feedd46e6bfd776979f7a
Jonas Nathanael Fleischer <jonas_nathanael.fleischer@stud.htwk-leipzig.de>
2024-11-06 22:14:02 +0100

    run generation

---
+++ src/api/generated/models/index.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreatePluginRequest';
+export * from './CreatePluginResponse';
+export * from './CreateProjectRequest';
+export * from './CreateProjectResponse';
+export * from './CreateUserRequest';
+export * from './CreateUserResponse';
+export * from './DeleteGlobalPluginResponse';
+export * from './GetAllUsersResponse';
+export * from './GetGlobalPluginResponse';
+export * from './GetPluginResponse';
+export * from './GetProjectResponse';
+export * from './GetProjectsResponse';
+export * from './GetUserResponse';
+export * from './LoginRequest';
+export * from './LoginResponse';
+export * from './PatchGlobalPluginRequest';
+export * from './PatchUserRequest';
+export * from './UpdateProjectPluginRequest';
