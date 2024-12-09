b7e52dab1e1387800066934cacb945bf8d18ad3f
Oscar Friske <oscar.friske@stud.htwk-leipzig.de>
2024-12-09 10:09:43 +0100

    add unit tests

--- src/views/SplitView/__test__/SplitView.test.ts
+++ src/views/SplitView/__test__/SplitView.test.ts
@@ -3,8 +3,13 @@ import { mount } from '@vue/test-utils';
 import SplitView from '../SplitView.vue';
 import { createPinia, setActivePinia } from 'pinia';
 import router from '@/router';
+import { Splitpanes } from 'splitpanes';
 
 setActivePinia(createPinia());
+
+beforeEach(() => {
+  localStorage.clear();
+});
 
 describe('SplitView.vue', () => {
   it('renders correctly', () => {
@@ -24,5 +29,88 @@ describe('SplitView.vue', () => {
 
     expect(wrapper.findAll('.splitpanes__pane')[0].isVisible()).toBeTruthy();
     expect(wrapper.findAll('.splitpanes__pane')[1].isVisible()).toBeTruthy();
+  });
+
+  it('saves pane sizes to localStorage on resize', async () => {
+    const wrapper = mount(SplitView, {
+      global: {
+        stubs: {
+          ProjectSearchView: {
+            template: '<span />',
+          },
+          ProjectView: {
+            template: '<span />',
+          },
+        },
+        plugins: [router],
+      },
+    });
+
+    const newSizes = [
+      { min: 20, max: 100, size: 20 },
+      { min: 32, max: 100, size: 80 },
+    ];
+    const pane = wrapper.findComponent(Splitpanes);
+    pane.vm.$emit('resized', newSizes);
+
+    const storedSizes = localStorage.getItem('paneSizes');
+    expect(storedSizes).toBe(JSON.stringify(newSizes));
+  });
+
+  it('should save pane sizes to localStorage on resize', async () => {
+    const wrapper = mount(SplitView, {
+      global: {
+        stubs: {
+          ProjectSearchView: {
+            template: '<span />',
+          },
+          ProjectView: {
+            template: '<span />',
+          },
+        },
+        plugins: [router],
+      },
+    });
+
+    const newSizes = [
+      { min: 20, max: 100, size: 20 },
+      { min: 32, max: 100, size: 80 },
+    ];
+    const pane = wrapper.findComponent(Splitpanes);
+    pane.vm.$emit('resized', newSizes);
+
+    const storedSizes = localStorage.getItem('paneSizes');
+    expect(storedSizes).toBe(JSON.stringify(newSizes));
+  });
+
+  it('should load pane sizes from localStorage on mount', () => {
+    const mockPaneSizes = [
+      { min: 20, max: 100, size: 20 },
+      { min: 32, max: 100, size: 80 },
+    ];
+    const getItemSpy = vi
+      .spyOn(Storage.prototype, 'getItem')
+      .mockReturnValue(JSON.stringify(mockPaneSizes));
+
+    const wrapper = mount(SplitView, {
+      global: {
+        stubs: {
+          ProjectSearchView: {
+            template: '<span />',
+          },
+          ProjectView: {
+            template: '<span />',
+          },
+        },
+        plugins: [router],
+      },
+    });
+
+    const leftPaneWidth = wrapper.vm.leftPaneWidth;
+    const rightPaneWidth = wrapper.vm.rightPaneWidth;
+
+    expect(getItemSpy).toHaveBeenCalledWith('paneSizes');
+    expect(leftPaneWidth).toBe(mockPaneSizes[0].size);
+    expect(rightPaneWidth).toBe(mockPaneSizes[1].size);
   });
 });
