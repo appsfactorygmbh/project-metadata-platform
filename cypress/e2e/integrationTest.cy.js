f4c2c3771c5c194b6476ea96408709b6be49490b
Jonas Nathanael Fleischer <jonas_nathanael.fleischer@stud.htwk-leipzig.de>
2025-01-17 11:51:36 +0100

    Enhance integration login screen test by adding a wait time for improved stability

--- cypress/e2e/integrationTest.cy.js
+++ cypress/e2e/integrationTest.cy.js
@@ -1,6 +1,7 @@
 describe('Login Screen', () => {
   it('should load correctly', () => {
     cy.visit('http://127.0.0.1:80/login');
+    cy.wait(1000);
     cy.get('h1').should('contain', 'Login');
     cy.get('input[id="standard_login_email"]').should('exist');
     cy.get('input[id="standard_login_password"]').should('exist');
