2460836a849e3b9c19aacb98dc770bb82f26c56f
Finn Wulfert <finn.wulfert@appsfactory.de>
2026-06-23 11:56:15 +0200

    feat(backend): Adds Handler for Authorization Exceptions

---
+++ src/backend/ProjectMetadataPlatform.Domain/Errors/AuthorizationExceptions/UnauthorizedException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectMetadataPlatform.Domain.Errors.AuthorizationExceptions;
+
+/// <summary>
+/// Exception thrown when access is unauthorized.
+/// </summary>
+public class UnauthorizedException()
+    : AuthorizationException("Actor is unauthorized to access this ressource.");
