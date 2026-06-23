7084fdc4e174d59dc72516f95fa2a050af0d50da
Finn Wulfert <finn.wulfert@appsfactory.de>
2026-06-23 13:49:25 +0200

    feat(backend)!: Adds PipelineBehavior and Interceptor for Authorization Enforcement
    Now throws Unauthorized by default if Authorization Tracker wasnt checked

---
+++ src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationEnforcerBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ProjectMetadataPlatform.Application.Interfaces;
+using ProjectMetadataPlatform.Domain.Errors.AuthorizationExceptions;
+
+namespace ProjectMetadataPlatform.Application.Authorization;
+
+public class AuthorizationEnforcerBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull()
+{
+    private readonly IAuthorizationTracker _tracker;
+
+    public AuthorizationEnforcerBehavior(IAuthorizationTracker tracker)
+    {
+        _tracker = tracker;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        var response = await next(cancellationToken);
+        if (_tracker.WasChecked)
+        {
+            return response;
+        }
+        else
+        {
+            throw new UnauthorizedException();
+        }
+    }
+}
---
+++ src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationTracker.cs
@@ -0,0 +1,13 @@
+﻿using ProjectMetadataPlatform.Application.Interfaces;
+
+namespace ProjectMetadataPlatform.Application.Authorization;
+
+public class AuthorizationTracker : IAuthorizationTracker
+{
+    public bool WasChecked { get; private set; }
+
+    public void MarkAsChecked()
+    {
+        WasChecked = true;
+    }
+}
