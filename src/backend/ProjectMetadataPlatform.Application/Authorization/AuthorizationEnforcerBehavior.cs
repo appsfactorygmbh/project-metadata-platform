4916d4d478bc4573eb476003c04e14de8eb28284
Finn Wulfert <finn.wulfert@appsfactory.de>
2026-07-07 08:52:59 +0200

    chore(backend): revert authorization state in handler
    Now reverts authorization check tracker in pipeline behavior to handle multiple requests per api call;

--- src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationEnforcerBehavior.cs
+++ src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationEnforcerBehavior.cs
@@ -6,17 +6,34 @@ using ProjectMetadataPlatform.Domain.Errors.AuthorizationExceptions;
 
 namespace ProjectMetadataPlatform.Application.Authorization;
 
+/// <summary>
+/// Pipeline Behavior that checks whether authorization was checked in a request.
+/// </summary>
+/// <typeparam name="TRequest">Request to be handled.</typeparam>
+/// <typeparam name="TResponse">Response from Handler.</typeparam>
 public class AuthorizationEnforcerBehavior<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull()
+    where TRequest : notnull
 {
     private readonly IAuthorizationTracker _tracker;
 
+    /// <summary>
+    /// Constructor for <see cref="AuthorizationEnforcerBehavior{TRequest,TResponse}"/>
+    /// </summary>
+    /// <param name="tracker">Tracks Authorization Status.</param>
     public AuthorizationEnforcerBehavior(IAuthorizationTracker tracker)
     {
         _tracker = tracker;
     }
 
+    /// <summary>
+    /// Checks if Authorization was checked in the Request and resets it.
+    /// </summary>
+    /// <param name="request">Request to be handled.</param>
+    /// <param name="next">Next Pipeline Step.</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Response for the Request.</returns>
+    /// <exception cref="UnauthorizedException">Thrown if no Authorization check happened.</exception>
     public async Task<TResponse> Handle(
         TRequest request,
         RequestHandlerDelegate<TResponse> next,
@@ -26,6 +43,7 @@ public class AuthorizationEnforcerBehavior<TRequest, TResponse>
         var response = await next(cancellationToken);
         if (_tracker.WasChecked)
         {
+            _tracker.RevertCheck();
             return response;
         }
         else
--- src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationTracker.cs
+++ src/backend/ProjectMetadataPlatform.Application/Authorization/AuthorizationTracker.cs
@@ -2,12 +2,23 @@
 
 namespace ProjectMetadataPlatform.Application.Authorization;
 
+/// <summary>
+/// Implements <see cref="IAuthorizationTracker"/>
+/// </summary>
 public class AuthorizationTracker : IAuthorizationTracker
 {
+    /// <inheritdoc/>
     public bool WasChecked { get; private set; }
 
+    /// <inheritdoc/>
     public void MarkAsChecked()
     {
         WasChecked = true;
+    }
+
+    /// <inheritdoc/>
+    public void RevertCheck()
+    {
+        WasChecked = false;
     }
 }
